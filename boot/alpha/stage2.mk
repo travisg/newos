35105456e26c87119748766feee9b908a4ab8223
Travis Geiselbrecht <geist@newos.org>
2001-02-14 23:07:12 +0000

    Screwing with the alpha and bringing it a little closer to compiling.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@83 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- boot/alpha/stage2.mk
+++ boot/alpha/stage2.mk
@@ -1,31 +1,32 @@
-# sh4 stage2 makefile
+
 STAGE2_DIR = boot/$(ARCH)
-STAGE2_OBJS = $(STAGE2_DIR)/stage2.o
+STAGE2_OBJ_DIR = $(STAGE2_DIR)/$(OBJ_DIR)
+STAGE2_OBJS = $(STAGE2_OBJ_DIR)/stage2.o
 DEPS += $(STAGE2_OBJS:.o=.d)
 
 STAGE2 = boot/stage2
-STAGE2_ARCH = boot/$(ARCH)/stage2
+STAGE2_ARCH = $(STAGE2_OBJ_DIR)/stage2
 
-$(STAGE2): $(STAGE2_ARCH)
-	ln -sf ../$< $@  
-
-$(STAGE2_ARCH): $(STAGE2_OBJS)
-	$(LD) -dN --script=$(STAGE2_DIR)/stage2.ld -L $(LIBGCC_PATH) $< $(LIBGCC) -o $@
+$(STAGE2_ARCH): $(STAGE2_OBJS) $(LIBC)
+	$(LD) -dN --script=$(STAGE2_DIR)/stage2.ld -L $(LIBGCC_PATH) $(LIBGCC) $(STAGE2_OBJS) $(LIBC) -o $@
 
 stage2clean:
-	rm -f $(STAGE2_OBJS) $(STAGE2_DIR)/stage2
+	rm -f $(STAGE2_OBJS) $(STAGE2_ARCH)
 
-# 
-$(STAGE2_DIR)/%.o: $(STAGE2_DIR)/%.c 
+$(STAGE2_OBJ_DIR)/%.o: $(STAGE2_DIR)/%.c 
+	@mkdir -p $(STAGE2_OBJ_DIR)
 	$(CC) $(GLOBAL_CFLAGS) -Iinclude -I$(STAGE2_DIR) -c $< -o $@
 
-$(STAGE2_DIR)/%.d: $(STAGE2_DIR)/%.c
+$(STAGE2_OBJ_DIR)/%.d: $(STAGE2_DIR)/%.c
+	@mkdir -p $(STAGE2_OBJ_DIR)
 	@echo "making deps for $<..."
 	@(echo -n $(dir $@);$(CC) $(GLOBAL_CFLAGS) -Iinclude -I$(STAGE2_DIR) -M -MG $<) > $@
 
-$(STAGE2_DIR)/%.d: $(STAGE2_DIR)/%.S
+$(STAGE2_OBJ_DIR)/%.d: $(STAGE2_DIR)/%.S
+	@mkdir -p $(STAGE2_OBJ_DIR)
 	@echo "making deps for $<..."
 	@(echo -n $(dir $@);$(CC) $(GLOBAL_CFLAGS) -Iinclude -I$(STAGE2_DIR) -M -MG $<) > $@
 
-$(STAGE2_DIR)/%.o: $(STAGE2_DIR)/%.S
+$(STAGE2_OBJ_DIR)/%.o: $(STAGE2_DIR)/%.S
+	@mkdir -p $(STAGE2_OBJ_DIR)
 	$(CC) $(GLOBAL_CFLAGS) -Iinclude -I$(STAGE2_DIR) -c $< -o $@
