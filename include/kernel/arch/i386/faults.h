a7545e4c6bda674276636378bcd6396c0a992bbe
Travis Geiselbrecht <geist@newos.org>
2001-02-25 15:49:03 +0000

    More branches and edits.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@103 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- include/kernel/arch/i386/faults.h
+++ include/kernel/arch/i386/faults.h
@@ -1,9 +1,5 @@
-#ifndef _ARCH_FAULTS
-#define _ARCH_FAULTS
-
-#include <stage2.h>
-
-int arch_faults_init(kernel_args *ka);
+#ifndef _I386_FAULTS
+#define _I386_FAULTS
 
 int i386_general_protection_fault(int errorcode);
 int i386_double_fault(int errorcode);
---
+++ include/kernel/arch/i386/vm.h
@@ -0,0 +1,12 @@
+#ifndef _ARCH_VM_H
+#define _ARCH_VM_H
+
+#include <stage2.h>
+
+int arch_vm_init(kernel_args *ka);
+int arch_vm_init2(kernel_args *ka);
+int map_page_into_kspace(addr paddr, addr kaddr, int lock);
+int i386_page_fault(int cr2reg, unsigned int fault_address);
+
+#endif
+
