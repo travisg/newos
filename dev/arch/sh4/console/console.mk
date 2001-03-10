d5f874c5d9a2b0624a839913151fcc381c7166ed
Travis Geiselbrecht <geist@newos.org>
2001-03-10 15:59:47 +0000

    Make all of sh4 build build on sparc.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@117 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- dev/arch/sh4/console/console.mk
+++ dev/arch/sh4/console/console.mk
@@ -14,12 +14,12 @@ $(CONSOLE_DEV_OBJ_DIR)/%.o: $(CONSOLE_DEV_DIR)/%.c
 $(CONSOLE_DEV_OBJ_DIR)/%.d: $(CONSOLE_DEV_DIR)/%.c
 	@mkdir -p $(CONSOLE_DEV_OBJ_DIR)
 	@echo "making deps for $<..."
-	@(echo -n $(dir $@); $(CC) $(GLOBAL_CFLAGS) $(DEV_INCLUDES) -M -MG $<) > $@
+	@($(ECHO) -n $(dir $@); $(CC) $(GLOBAL_CFLAGS) $(DEV_INCLUDES) -M -MG $<) > $@
 
 $(CONSOLE_DEV_OBJ_DIR)/%.d: $(CONSOLE_DEV_DIR)/%.S
 	@mkdir -p $(CONSOLE_DEV_OBJ_DIR)
 	@echo "making deps for $<..."
-	@(echo -n $(dir $@);$(CC) $(GLOBAL_CFLAGS) $(DEV_INCLUDES) -M -MG $<) > $@
+	@($(ECHO) -n $(dir $@);$(CC) $(GLOBAL_CFLAGS) $(DEV_INCLUDES) -M -MG $<) > $@
 
 $(CONSOLE_DEV_OBJ_DIR)/%.o: $(CONSOLE_DEV_DIR)/%.S
 	@mkdir -p $(CONSOLE_DEV_OBJ_DIR)
