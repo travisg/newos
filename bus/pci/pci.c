90b501f59f82169c46ab73ade5c4b82e7ecaf31c
Travis Geiselbrecht <geist@newos.org>
2001-11-08 21:55:49 +0000

    I wonder how this worked before
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@296 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- bus/pci/pci.c
+++ bus/pci/pci.c
@@ -64,9 +64,9 @@ static int pci_config_compare_func(void *_config, void *_key)
 	char *key = _key;
 
 	if(!strcmp(config->full_path, key))
-		return 1;
-	else
 		return 0;
+	else
+		return -1;
 }	
 
 static int pci_open(const char *name, dev_cookie *_cookie)
