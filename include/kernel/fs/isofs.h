cc32206252ee6606cccae2d87cb0d34f35ab8cac
Travis Geiselbrecht <geist@newos.org>
2001-11-23 18:58:51 +0000

    oops! had forgotten this file in the iso9660 checkin
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@319 c25cc9d1-44fa-0310-b259-ad778cb1d433

---
+++ include/kernel/fs/isofs.h
@@ -0,0 +1,10 @@
+/*
+** Copyright 2001, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#ifndef _ISOFS_H
+#define _ISOFS_H
+
+int bootstrap_isofs();
+
+#endif
