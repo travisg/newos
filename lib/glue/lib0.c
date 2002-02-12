f09e7e586210f6fc7629d095b9a4bb35821cb586
Travis Geiselbrecht <geist@newos.org>
2002-02-12 00:27:15 +0000

    total hack to make gcc 3.x happy.
    when compiling the hoard code which uses c++, it always emits a reference to a function that's supposed to be in libstdc++, which we dont have here. 
    I've stuck in a null version of that function, with the idea that it'll never get called, since it's an exception utility function.
    
    
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@495 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- lib/glue/lib0.c
+++ lib/glue/lib0.c
@@ -35,3 +35,10 @@ void _call_ctors(void)
 	}
 }
 
+int __gxx_personality_v0(void);
+
+// XXX hack to make gcc 3.x happy until we get libstdc++ working
+int __gxx_personality_v0(void)
+{
+	return 0;
+}
