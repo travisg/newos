c3ea20e0cea7f00789e092091ea4e9c262347748
Travis Geiselbrecht <geist@newos.org>
2002-02-12 10:51:32 +0000

    roll in a change by Rob Judd that lets you change the debug com port to com2
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@502 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- kernel/arch/i386/arch_dbg_console.c
+++ kernel/arch/i386/arch_dbg_console.c
@@ -2,6 +2,9 @@
 ** Copyright 2001, Travis Geiselbrecht. All rights reserved.
 ** Distributed under the terms of the NewOS License.
 */
+/*
+** Modified 2001/09/05 by Rob Judd<judd@ob-wan.com>
+*/
 #include <kernel/kernel.h>
 #include <kernel/int.h>
 #include <kernel/arch/cpu.h>
@@ -13,26 +16,41 @@
 
 #define BOCHS_E9_HACK 0
 
+// Select between COM1 and COM2 for debug output
+#define USE_COM1 1
+
 static const int dbg_baud_rate = 115200;
 
 int arch_dbg_con_init(kernel_args *ka)
 {
 	short divisor = 115200 / dbg_baud_rate;
 
+#if USE_COM1
 	out8(0x80, 0x3fb);	/* set up to load divisor latch	*/
 	out8(divisor & 0xf, 0x3f8);		/* LSB */
 	out8(divisor >> 8, 0x3f9);		/* MSB */
 	out8(3, 0x3fb);		/* 8N1 */
+#else // COM2
+	out8(0x80, 0x2fb);	/* set up to load divisor latch	*/
+	out8(divisor & 0xf, 0x2f8);		/* LSB */
+	out8(divisor >> 8, 0x2f9);		/* MSB */
+	out8(3, 0x2fb);		/* 8N1 */
+#endif
 
 	return 0;
 }
 
 char arch_dbg_con_read(void)
 {
+#if USE_COM1
 	while ((in8(0x3fd) & 1) == 0)
 		;
-
 	return in8(0x3f8);
+#else
+	while ((in8(0x2fd) & 1) == 0)
+		;
+	return in8(0x2f8);
+#endif
 }
 
 static void _arch_dbg_con_putch(const char c)
@@ -40,10 +58,17 @@ static void _arch_dbg_con_putch(const char c)
 #if BOCHS_E9_HACK
 	out8(c, 0xe9);
 #else
+
+#if USE_COM1
 	while ((in8(0x3fd) & 0x20) == 0)
 		;
-
 	out8(c, 0x3f8);
+#else // COM2
+	while ((in8(0x2fd) & 0x20) == 0)
+		;
+	out8(c, 0x2f8);
+#endif
+
 #endif
 }
 
