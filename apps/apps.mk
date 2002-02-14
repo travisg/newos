47a9541da314dacb37faa0883c0f4fee9791f05e
Manuel Jesus Petit de Gabriel <freston@bayarea.net>
2002-02-14 00:21:28 +0000

    I'm really proud of this code... piece of art.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@518 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- apps/apps.mk
+++ apps/apps.mk
@@ -11,6 +11,7 @@ include $(APPS_DIR)/shell/app.mk
 include $(APPS_DIR)/testapp/app.mk
 include $(APPS_DIR)/vmtest/app.mk
 include $(APPS_DIR)/true/app.mk
+include $(APPS_DIR)/false/app.mk
 include $(APPS_DIR)/fibo/app.mk
 include $(APPS_DIR)/rld/app.mk
 include $(APPS_DIR)/rldtest/app.mk
---
+++ apps/false/app.mk
@@ -0,0 +1,28 @@
+FALSEAPP_DIR = $(APPS_DIR)/false
+FALSEAPP_OBJ_DIR = $(FALSEAPP_DIR)/$(OBJ_DIR)
+FALSEAPP_OBJS = \
+	$(FALSEAPP_OBJ_DIR)/main.o
+
+DEPS += $(FALSEAPP_OBJS:.o=.d)
+
+FALSEAPP = $(FALSEAPP_OBJ_DIR)/false
+
+$(FALSEAPP): $(FALSEAPP_OBJS) $(LIBS) $(GLUE)
+	$(LD) --script=$(APPS_LDSCRIPT) -L $(LIBGCC_PATH) -o $@ $(GLUE) $(FALSEAPP_OBJS) $(LINK_LIBS) $(LIBGCC)
+
+falseappclean:
+	rm -f $(FALSEAPP_OBJS) $(FALSEAPP)
+
+APPS += $(FALSEAPP)
+
+APPS_CLEAN += falseappclean
+
+$(FALSEAPP_OBJ_DIR)/%.o: $(FALSEAPP_DIR)/%.c
+	@if [ ! -d $(FALSEAPP_OBJ_DIR) ]; then mkdir -p $(FALSEAPP_OBJ_DIR); fi
+	@mkdir -p $(FALSEAPP_OBJ_DIR)
+	$(CC) -c $< $(GLOBAL_CFLAGS) $(APPS_INCLUDES) -o $@
+
+$(FALSEAPP_OBJ_DIR)/%.d: $(FALSEAPP_DIR)/%.c
+	@if [ ! -d $(FALSEAPP_OBJ_DIR) ]; then mkdir -p $(FALSEAPP_OBJ_DIR); fi
+	@echo "making deps for $<..."
+	@($(ECHO) -n $(dir $@); $(CC) $(GLOBAL_CFLAGS) $(APPS_INCLUDES) -M -MG $<) > $@
