b22ecc811194a65ae43aa21fb2495d5a3b531303
Manuel Jesus Petit de Gabriel <freston@bayarea.net>
2002-02-24 16:30:10 +0000

    I knew i was forgetting something.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@559 c25cc9d1-44fa-0310-b259-ad778cb1d433

---
+++ include/nulibc/sys/types.h
@@ -0,0 +1,7 @@
+/*
+** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+** Copyright 2002, Manuel J. Petit. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+
+#include <newos/types.h>
