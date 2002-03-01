5ce741266c17ad59ec66108534e7b9ebf1b39260
Travis Geiselbrecht <geist@newos.org>
2002-03-01 21:44:42 +0000

    As usual, forgot a couple of files
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@579 c25cc9d1-44fa-0310-b259-ad778cb1d433

---
+++ boot/m68k/stage2_text.c
@@ -0,0 +1,26 @@
+#include "stage2_priv.h"
+
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void puts(const char *str)
+{
+	while (*str)
+		putc(*str++);
+}
+
+int dprintf(const char *fmt, ...)
+{
+	int ret;
+	va_list args;
+	char temp[256];
+
+	va_start(args, fmt);
+	ret = vsprintf(temp,fmt,args);
+	va_end(args);
+
+	puts(temp);
+	return ret;
+}
+
