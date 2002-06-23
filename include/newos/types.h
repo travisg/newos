d66057e85ff0b1224e9856ef2f5c38708a84ce41
Travis Geiselbrecht <geist@newos.org>
2002-06-23 13:19:54 +0000

    cleaned up the newos/types.h file that everyone has to tweak to get around the infamous size_t problem. Make it a lot easier to tweak it. Not a solution, but I was tired of having this file checked out.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@658 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- include/newos/types.h
+++ include/newos/types.h
@@ -51,24 +51,58 @@ typedef int bool;
    future.
 */
 
-#ifdef __BEOS__
+#ifdef ARCH_m68k
+#define __SIZE_T_LONG       1
+#endif
+
+#ifdef ARCH_ppc
+#define __SIZE_T_LONG       1
+#endif
+
+#ifdef ARCH_sh4
+#define __SIZE_T_INT        1
+#endif
+
+#if !__SIZE_T_LONG && !__SIZE_T_INT
+/* when in doubt, set it to LONG */
+#define __SIZE_T_LONG       1
+#endif
+
+/* uncomment the following if you want to override LONG vs INT */
+#if 0
+#ifdef __SIZE_T_LONG
+#undef __SIZE_T_LONG
+#endif
+#ifdef __SIZE_T_INT
+#undef __SIZE_T_INT
+#endif
+
+/* to override the LONG vs INT setting, set *one* of the below to 1 */
+#define __SIZE_T_LONG       0
+#define __SIZE_T_INT        1
+#endif
+
+#if __SIZE_T_LONG
 typedef unsigned long       size_t;
 typedef signed long         ssize_t;
+#elif __SIZE_T_INT
+typedef unsigned int        size_t;
+typedef signed int          ssize_t;
 #else
-typedef unsigned long        size_t;
-typedef signed long          ssize_t;
+#error "Don't know what size_t should be (int or long)!"
 #endif
+
 typedef int64               off_t;
 
-typedef unsigned char		u_char;
-typedef unsigned short		u_short;
-typedef unsigned int		u_int;
-typedef unsigned long		u_long;
+typedef unsigned char       u_char;
+typedef unsigned short      u_short;
+typedef unsigned int        u_int;
+typedef unsigned long       u_long;
 
-typedef unsigned char		uchar;
-typedef unsigned short		ushort;
-typedef unsigned int		uint;
-typedef unsigned long		ulong;
+typedef unsigned char       uchar;
+typedef unsigned short      ushort;
+typedef unsigned int        uint;
+typedef unsigned long       ulong;
 
 #endif
 
