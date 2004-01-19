5fa72c9f08950c22cfdc557d164599e0a961b0e5
Travis Geiselbrecht <geist@newos.org>
2004-01-19 19:33:29 +0000

    A couple of additions to the syscall-timing function.
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@1048 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- apps/testapp/misctests.cpp
+++ apps/testapp/misctests.cpp
@@ -1,5 +1,5 @@
 /*
-** Copyright 2003, Travis Geiselbrecht. All rights reserved.
+** Copyright 2004, Travis Geiselbrecht. All rights reserved.
 ** Distributed under the terms of the NewOS License.
 */
 #include <string.h>
@@ -11,7 +11,7 @@ int syscall_bench(int arg)
 {
 	bigtime_t start_time = _kern_system_time();
 	int i;
-	const int count = 50000;
+	const int count = 250000;
 
 	for(i=0; i < count; i++) {
 		_kern_null();
@@ -21,6 +21,7 @@ int syscall_bench(int arg)
 
 	printf("took %Ld usecs to call _kern_null() %d times (%Ld usecs/call)\n", 
 		start_time, count, start_time/count);
+	printf("%Ld cycles/syscall on 500 Mhz cpu\n", start_time * 500 / count);
 
 	return 0;
 }
