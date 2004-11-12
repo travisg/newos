0067a2a739bdc4aea656fa4f2f3649ec6c0b0e99
Travis Geiselbrecht <geist@newos.org>
2004-11-12 21:03:00 +0000

    Check in this new makesystem macro before I forget it
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@1138 c25cc9d1-44fa-0310-b259-ad778cb1d433

--- macros.mk
+++ macros.mk
@@ -8,3 +8,5 @@ FINDINLIST = $(if $(findstring $(1),$(2)),1,0)
 # used in template files to build a directory in the build rules
 MKDIR = if [ ! -d $(dir $@) ]; then mkdir -p $(dir $@); fi
 
+# gets the local directory of the makefile. Requires gmake 3.80
+GET_LOCAL_DIR    = $(patsubst %/,%,$(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))))
