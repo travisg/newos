c5af0cff16a24773a15d0fd32cc59d06dcee573c
Travis Geiselbrecht <geist@newos.org>
2005-06-20 00:19:59 +0000

    A little bit of work in progress on x86_64
    git-svn-id: svn+ssh://newos.org/var/svn/newos/newos@1166 c25cc9d1-44fa-0310-b259-ad778cb1d433

---
+++ include/arch/x86_64/endian.h
@@ -0,0 +1,11 @@
+/*
+** Copyright 2004, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+#ifndef _NEWOS_ARCH_X86_64_ENDIAN_H
+#define _NEWOS_ARCH_X86_64_ENDIAN_H
+
+#define BYTE_ORDER LITTLE_ENDIAN
+
+#endif
+
